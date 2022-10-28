16ebdd60f0c4e9635ec565d5a674024fe83f7bd7
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-10-28 21:48:38 +0300

    returns array of author instances

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,4 @@
+require_relative './article'
 class Magazine
   attr_accessor :name, :category
   @@all = []
@@ -7,5 +8,33 @@ class Magazine
     @category = category
     @@all << self
   end
+
+  def self.all
+    @@all
+    end
+
+    #contributors
+    #return array of Author instances who have written for this magazine
+    def contributors
+      contributed_articles.map do | my_articles |
+        my_articles.author
+        end
+        end
+
+
+    #find by name
+    #def self.find_by_name()
+    
+
+    #title of article
+    contributed_articles.map do | my_articles |
+      my_articles.title
+    end
+    end
+
+
+    #contributing authors
+
+    #articles for magazines
 
 end
